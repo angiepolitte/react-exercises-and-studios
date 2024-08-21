05677e338e6f06deedfa71de515a70f623ed5bdd
CandyQueen77 <angiepolitte@gmail.com>
2024-08-21 15:00:12 -0500

    Part 1 Exercises first commit

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,18 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "Random popular books at the moment";
+  let book1 =
+    "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcS3j9tPPCLuet5ArO4YwbrRpzxn-o7mTyped8Vx70niDqffFE2Y-eHpzNcgI65Uw11rkz91nepp5xlqXWGWZfRsYuf0YiyEnUZ_nA4M-Yw&usqp=CAc";
+  let book2 =
+    "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR-ka1YUrvpP8BsTywtqe2HubotMNID7gSdsLDJ5Az_-Qmm3NrpOsDiEFoFNZvBPRXRkiAD0XW2KHZ4jB2JNbASBPzhok_EF-n6dCvcAUQ1b3V_i3Wr8m9I3Q&usqp=CAc";
+  let book3 =
+    "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTYqHXH8TMtKUrBAb_xVJ0lfZptJ1zA6xKZJiJT5a4GKozly0RLM8Is7I0few5H5rpCUU8_9pUwos-8DVoTyGdad6779I_LgS5_NRKSz1E&usqp=CAc";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="How to hold a cockroach" />
+      <img src={book2} alt="The courage to be disliked" />
+      <img src={book3} alt="surrounded by idiots" />
+    </div>
+  );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,31 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from "./ChoresList.module.css";
+
+export default function ChoresList() {
+  return <p>No content is here!</p>;
 }
+// export default function ChoresList() {
+//   return (
+//     <div>
+//       <h1 class="choresHeading">Chores to complete this week</h1>
+//       <p class="choresText">No content is here!</p>;
+//     </div>
+//   );
+// }
+
+// class MovieList extends React.Component {
+//    render() {
+//       const movies = ["Pride and Prejudice", "Howl's Moving Castle"];
+//       return (
+//       <div>
+//          <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
+//          <ol>
+//             <li className={classes.movieText}>{movies[0]}</li>
+//             <li className={classes.movieText}>{movies[1]}</li>
+//          </ol>
+//       </div>
+//       );
+//    }
+// }
+
+// export default ChoresList;
